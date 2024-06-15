a4856b2df7354cb88b2b8648ed7d951090b1331e
kshitijgithb <kshitijkumargupta2017@gmail.com>
2024-06-16 00:59:20 +0530

    updating portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/myPortfolio/",
+  base: "/Portfolio/",
   plugins: [react()],
   css: {
     modules: {
